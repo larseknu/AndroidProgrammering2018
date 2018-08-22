d1eb1551314494e08edf8f45013b207807e5a73b
Lars Emil Knudsen <larsemil87@gmail.com>
2018-08-22 10:46:19 +0200

    PlayingWithIntents example updated

--- Lecture02_Intents/AfterLecture/PlayingWithIntents/app/src/main/java/com/example/larseknu/playingwithintents/MainActivity.java
+++ Lecture02_Intents/AfterLecture/PlayingWithIntents/app/src/main/java/com/example/larseknu/playingwithintents/MainActivity.java
@@ -5,31 +5,62 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    //TODO X4: Create a constant (public static final) to represent the key in the extra for the intent
+    public static final String KEY_NAME = "name";
+
+    //TODO Z1: Create a field variable for the button navigateToOtherButton
+    private Button navigateToOtherButton;
+    //TODO X2: Create a field variable (nameEditText) for the EditText view
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    //TODO 4: Add a navigateToOtherActivity(View view), method for a button-click
-    //TODO 5: Create an Intent to start OtherActivity in the navigate... method
-    //TODO 6: Start OtherActivity with the intent
+        // TODO X3: Instantiate nameEditText
+        nameEditText = findViewById(R.id.nameEditText);
 
-    public void navigateToOtherActivity(View view) {
-        Intent intent = new Intent(this, OtherActivity.class);
+        // TODO Z2: Instantiate navigateToOtherButton
+        navigateToOtherButton = findViewById(R.id.navigateToOtherButton);
 
-        startActivity(intent);
+        // TODO 4: Add a onClickListener to the navigateToOtherButton
+        navigateToOtherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO 5: Create an Intent to start OtherActivity in the navigate... method
+                Intent intent = new Intent(view.getContext(), OtherActivity.class);
+
+                // TODO X4: Get the text from the EditText when navigateToOtherActivity is ran
+                String name = nameEditText.getText().toString();
+
+                // TODO X5: Put the text from the EditText into the extra information in the Intent
+                intent.putExtra(KEY_NAME, name);
+
+                // TODO 6: Start OtherActivity with the intent
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void openWebPage(View view) {
-
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
         Uri uri = Uri.parse("http://hiof.no");
         intent.setData(uri);
+
+        startActivity(intent);
+    }
+
+    public void openMaps(View view) {
+        //Uri uri = Uri.parse("https://maps.google.com?q=59.128708,11.353176");
+        Uri uri = Uri.parse("geo:59.128708,11.353176");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 
         startActivity(intent);
     }
--- Lecture02_Intents/AfterLecture/PlayingWithIntents/app/src/main/java/com/example/larseknu/playingwithintents/OtherActivity.java
+++ Lecture02_Intents/AfterLecture/PlayingWithIntents/app/src/main/java/com/example/larseknu/playingwithintents/OtherActivity.java
@@ -1,19 +1,63 @@
 package com.example.larseknu.playingwithintents;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 public class OtherActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_GET = 9001;
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        // TODO X6: Get a reference to the intent we receive from MainActivity
+        Intent receivedIntent = getIntent();
+
+        // TODO X7: Get the extra "name" information from the intent
+        String name = receivedIntent.getStringExtra(MainActivity.KEY_NAME);
+
+        // TODO X8: Fill the TextView with the String from the extra-data
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(name);
+
+        imageView = findViewById(R.id.imageView);
     }
 
     public void getPicture(View view) {
-        Toast.makeText(this, "Getting picture", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "Getting picture", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMAGE_GET);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
+            try {
+                Uri fullPhotoUri = data.getData();
+                Bitmap picture = MediaStore.Images.Media.getBitmap(getContentResolver(), fullPhotoUri);
+                imageView.setImageBitmap(picture);
+            } catch (IOException e) {
+                Toast.makeText(this, "Couldn't get picture", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
