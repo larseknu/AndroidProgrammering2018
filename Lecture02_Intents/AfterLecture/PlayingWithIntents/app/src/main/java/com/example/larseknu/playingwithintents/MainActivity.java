8181001bcd464b7c72b5a2a878be77313710264a
Lars Emil Knudsen <larsemil87@gmail.com>
2018-08-22 11:42:27 +0200

    Finished the AfterLecture PlayingWithIntents example

--- Lecture02_Intents/AfterLecture/PlayingWithIntents/app/src/main/java/com/example/larseknu/playingwithintents/MainActivity.java
+++ Lecture02_Intents/AfterLecture/PlayingWithIntents/app/src/main/java/com/example/larseknu/playingwithintents/MainActivity.java
@@ -9,12 +9,12 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    //TODO X4: Create a constant (public static final) to represent the key in the extra for the intent
+    //TODO 2.4: Create a constant (public static final) to represent the key in the extra for the intent
     public static final String KEY_NAME = "name";
 
-    //TODO Z1: Create a field variable for the button navigateToOtherButton
+    //TODO 1.3: Create a field variable for the button navigateToOtherButton
     private Button navigateToOtherButton;
-    //TODO X2: Create a field variable (nameEditText) for the EditText view
+    //TODO 2.2: Create a field variable (nameEditText) for the EditText view
     private EditText nameEditText;
 
     @Override
@@ -22,46 +22,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO X3: Instantiate nameEditText
+        // TODO 2.3: Instantiate nameEditText
         nameEditText = findViewById(R.id.nameEditText);
 
-        // TODO Z2: Instantiate navigateToOtherButton
+        // TODO 1.4: Instantiate navigateToOtherButton
         navigateToOtherButton = findViewById(R.id.navigateToOtherButton);
 
-        // TODO 4: Add a onClickListener to the navigateToOtherButton
+        // TODO 1.5: Add a onClickListener to the navigateToOtherButton
         navigateToOtherButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO 5: Create an Intent to start OtherActivity in the navigate... method
+                // TODO 1.6: Create an Intent to start OtherActivity in the navigate... method
                 Intent intent = new Intent(view.getContext(), OtherActivity.class);
 
-                // TODO X4: Get the text from the EditText when navigateToOtherActivity is ran
+                // TODO 2.5: Get the text from the EditText when navigateToOtherActivity is ran
                 String name = nameEditText.getText().toString();
 
-                // TODO X5: Put the text from the EditText into the extra information in the Intent
+                // TODO 2.6: Put the text from the EditText into the extra information in the Intent
                 intent.putExtra(KEY_NAME, name);
 
-                // TODO 6: Start OtherActivity with the intent
+                // TODO 1.7: Start OtherActivity with the intent
                 startActivity(intent);
             }
         });
 
     }
 
+    // TODO 3.3: Create openWebPage(View view) method to handle the click
     public void openWebPage(View view) {
+        // TODO 3.4: Create an Intent
         Intent intent = new Intent();
+        // TODO 3.5: Set action ACTION_VIEW to the intent
         intent.setAction(Intent.ACTION_VIEW);
+        // TODO 3.6: Create an URI to a webpage and set it as data for the intent
         Uri uri = Uri.parse("http://hiof.no");
         intent.setData(uri);
-
+        // TODO 3.7: Start the activity with the intent
         startActivity(intent);
     }
 
+    // TODO 4.3: Create openMaps(View view) method to handle the click
     public void openMaps(View view) {
+        // TODO 4.4: Create an URI to https://maps.google.com?q=59.128708,11.353176
         //Uri uri = Uri.parse("https://maps.google.com?q=59.128708,11.353176");
+
+        // TODO 4.7: Create an URI with the geo:lan,lot instead
         Uri uri = Uri.parse("geo:59.128708,11.353176");
+
+        // TODO 4.5: Create an Intent with ACTION_VIEW and the URI
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 
+        // TODO 4.6: Start the activity with the intent
         startActivity(intent);
     }
 }
--- Lecture02_Intents/AfterLecture/PlayingWithIntents/app/src/main/java/com/example/larseknu/playingwithintents/OtherActivity.java
+++ Lecture02_Intents/AfterLecture/PlayingWithIntents/app/src/main/java/com/example/larseknu/playingwithintents/OtherActivity.java
@@ -15,9 +15,10 @@ import android.widget.Toast;
 import java.io.IOException;
 
 public class OtherActivity extends AppCompatActivity {
-
+    // TODO 5.9: Create a constant integer for the image request
     private static final int REQUEST_IMAGE_GET = 9001;
 
+    // TODO 5.4: Create a field variable for the ImageView
     private ImageView imageView;
 
     @Override
@@ -25,33 +26,42 @@ public class OtherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
 
-        // TODO X6: Get a reference to the intent we receive from MainActivity
+        // TODO 2.7: Get a reference to the intent we receive from MainActivity
         Intent receivedIntent = getIntent();
 
-        // TODO X7: Get the extra "name" information from the intent
+        // TODO 2.8: Get the extra "name" information from the intent
         String name = receivedIntent.getStringExtra(MainActivity.KEY_NAME);
 
-        // TODO X8: Fill the TextView with the String from the extra-data
+        // TODO 2.9: Fill the TextView with the String from the extra-data
         TextView textView = findViewById(R.id.textView);
         textView.setText(name);
 
+        // TODO 5.5: Instantiate the ImageView
         imageView = findViewById(R.id.imageView);
     }
 
+    // TODO 5.6: Create getPicture(View view) method to handle the click
     public void getPicture(View view) {
         //Toast.makeText(this, "Getting picture", Toast.LENGTH_SHORT).show();
-
+        // TODO 5.7: Create Intent with ACTION_GET_CONTENT
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        // TODO 5.8: Set the type of the intent to image/*
         intent.setType("image/*");
+        // TODO 5.10: Call the setActivityResult with the intent and the int constant
         startActivityForResult(intent, REQUEST_IMAGE_GET);
     }
 
+    // TODO 5.11: Override onActivityResult to handle result of the image request
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO 5.12: Check that the requestCode matches the int constant, and that the resultCode is OK
         if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
             try {
+                // TODO 5.13: Get the data URI from the intent
                 Uri fullPhotoUri = data.getData();
+                // TODO 5.14: Retrieve a bitmap of the picture from the URI, with the help of MediaStore.Images.Media.getBitMap
                 Bitmap picture = MediaStore.Images.Media.getBitmap(getContentResolver(), fullPhotoUri);
+                // TODO 5.15: Set the bitmap to the imageView
                 imageView.setImageBitmap(picture);
             } catch (IOException e) {
                 Toast.makeText(this, "Couldn't get picture", Toast.LENGTH_SHORT).show();
