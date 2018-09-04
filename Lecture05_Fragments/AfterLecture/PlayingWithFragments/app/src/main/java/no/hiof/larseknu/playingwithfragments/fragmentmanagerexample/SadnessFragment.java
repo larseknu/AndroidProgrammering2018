47de8cd6d7f99953ba73db6b929a11af4b6bb22f
Lars Emil Knudsen <larsemil87@gmail.com>
2018-09-04 07:50:49 +0200

    Refactored and added Fragment lifecycle events in SadnessFragment

--- Lecture05_Fragments/AfterLecture/PlayingWithFragments/app/src/main/java/no/hiof/larseknu/playingwithfragments/fragmentmanagerexample/SadnessFragment.java
+++ Lecture05_Fragments/AfterLecture/PlayingWithFragments/app/src/main/java/no/hiof/larseknu/playingwithfragments/fragmentmanagerexample/SadnessFragment.java
@@ -3,58 +3,15 @@ package no.hiof.larseknu.playingwithfragments.fragmentmanagerexample;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import no.hiof.larseknu.playingwithfragments.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SadnessFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SadnessFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-
-    public SadnessFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SadnessFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SadnessFragment newInstance(String param1, String param2) {
-        SadnessFragment fragment = new SadnessFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final String TAG = "SadnessFragment";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,4 +20,66 @@ public class SadnessFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_sadness, container, false);
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.d(TAG, "onActivityCreated");
+
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(TAG, "onStart");
+
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(TAG, "onStop");
+
+        super.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(TAG, "onPause");
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "onResume");
+
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.d(TAG, "onDestroyView");
+
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "onDetach");
+
+        super.onDetach();
+    }
 }
