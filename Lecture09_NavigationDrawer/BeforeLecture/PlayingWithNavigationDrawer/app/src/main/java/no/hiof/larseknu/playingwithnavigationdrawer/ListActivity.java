824f1b9495c4951ee4674ca6479bf68a33e5fa96
Lars Emil Knudsen <larsemil87@gmail.com>
2018-09-24 13:15:07 +0200

    BeforeLecture NavigationDrawer

---
+++ Lecture09_NavigationDrawer/BeforeLecture/PlayingWithNavigationDrawer/app/src/main/java/no/hiof/larseknu/playingwithnavigationdrawer/ListActivity.java
@@ -0,0 +1,14 @@
+package no.hiof.larseknu.playingwithnavigationdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
---
+++ Lecture09_NavigationDrawer/BeforeLecture/PlayingWithNavigationDrawer/app/src/main/java/no/hiof/larseknu/playingwithnavigationdrawer/MainActivity.java
@@ -0,0 +1,19 @@
+package no.hiof.larseknu.playingwithnavigationdrawer;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
