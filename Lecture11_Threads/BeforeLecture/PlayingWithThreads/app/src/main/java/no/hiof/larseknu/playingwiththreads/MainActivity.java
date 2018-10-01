fbecbe47b774a4efe98578ef053c48b0690f3d3d
Lars Emil Knudsen <larsemil87@gmail.com>
2018-10-01 11:37:01 +0200

    Added BeforeLecture example for PlayingWithThreads

---
+++ Lecture11_Threads/BeforeLecture/PlayingWithThreads/app/src/main/java/no/hiof/larseknu/playingwiththreads/MainActivity.java
@@ -0,0 +1,56 @@
+package no.hiof.larseknu.playingwiththreads;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView statusText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        statusText = findViewById(R.id.statusText);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.easyPermissionActivity:
+                //startActivity(new Intent(this, EasyPermissionActivity.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
